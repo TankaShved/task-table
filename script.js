ad067c7db2623b1b4d3f921d1107b9c7c1dbf77b
TanyaShved <tankonatanke90@gmail.com>
2020-04-23 19:59:56 +0300

    post not work

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// //1 vers. async await function.
-// async function main() {
-//   const n = await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users');
-//   const d = await n.json()
-//   console.log(d)
-// }
-// main()
-
 // //2 vers. fetch.
 // fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users')
 //   .then(response => response.json())
 //   .then(users => alert(users));
 
-// //3 vers. async =>.
-// let f = async () => {
-//   const n = await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users');
-//   const d = await n.json()
-//   console.log(d)
-// }
-// f()
 
 function user_get(value, string) {
   const cell = document.createElement('td')
@@ -36,13 +21,7 @@ function user_get(value, string) {
 //   string.append(but_del)
 // }
 
-// function user_post(){
-//   const user_add = [
-//     {document.querySelector('.user-age'): "31",
-//     "user_name": "Test",
-//     "user_sex": "M"
-//   ]
-// }
+// function user_post(){}
 
 async function get() {
   const url = 'https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users'
@@ -51,40 +30,61 @@ async function get() {
 
   const table_body = document.querySelector('tbody')
 
+  const string = document.createElement('tr')
+  table_body.append(string);
+
   for (let i = 0; i < data.length; i++) {
-    const string = document.createElement('tr')
-    table_body.append(string);
 
     user_get(data[i].user_age, string);
     user_get(data[i].user_name, string);
     user_get(data[i].user_sex, string);
-    // button_delete(string);
 
+    // button_delete(string);
     const but_del = document.createElement('input')
     but_del.type = 'button'
     but_del.value = 'delete'
     but_del.onclick = function () {
-      async function delUser() {
-        const noga = data[i].id
-        await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users/' + noga, { method: 'DELETE' })
-        table_body.innerHTML = ""
+      async function user_delete() {
+        const a = data[i].id
+        await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users/' + a,
+          { method: 'DELETE' })
+        table_body.remove()
         get()
       }
-      delUser()
-
+      user_delete()
     }
     string.append(but_del)
-
   }
-  console.log(data)
 
+  // ADD
+  const but_add = document.querySelector('.add_user')
+  but_add.onclick = function () {
+    const val_age = document.querySelector('.user_age').value
+    const val_name = document.querySelector('.user_name').value
+    const val_sex = document.querySelector('.user_sex').value
 
-  //for testing
+    let user = {
+      name: val_name,
+      sex: val_sex,
+      age: val_age,
+    }
 
-  // const exampleId = "3e28ea39-1b0e-4e76-82b9-7985c5108e20"
-  // const dsf = await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users/' + exampleId,
-  //   { method: 'DELETE' })
+    async function user_add() {
+      let response = await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+      });
 
+      let result = await response.json();
+      console.log(result);
+    }
+    user_add()
+    
+  }
+  //console.log(data)
 }
 get()
 
