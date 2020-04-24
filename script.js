35db62c02e7c5143dbaae7d984d94f3c70d04f2c
TanyaShved <tankonatanke90@gmail.com>
2020-04-24 11:34:01 +0300

    button edit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function user_cell(value, string) {
   const cell = document.createElement('td')
   string.append(cell)
   cell.append(value)
+  return cell
 }
 
 //get
@@ -17,18 +18,20 @@ async function get() {
     const string = document.createElement('tr')
     table_body.append(string);
 
-    user_cell(data[i].user_age, string);
-    user_cell(data[i].user_name, string);
-    user_cell(data[i].user_sex, string);
+    const cell_age = user_cell(data[i].user_age, string);
+    const cell_name = user_cell(data[i].user_name, string);
+    const cell_sex = user_cell(data[i].user_sex, string);
 
     //button delete
     const button_delete = document.createElement('input')
     button_delete.type = 'button'
     button_delete.value = 'delete'
 
+    const a = data[i].id // users id
+
     button_delete.onclick = function () {
       async function user_delete() {
-        const a = data[i].id // users id
+
         let response = await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users/' + a,
           { method: 'DELETE' })
         if (response.ok) {
@@ -42,6 +45,54 @@ async function get() {
       user_delete()
     }
     string.append(button_delete)
+
+    //button edit
+    const button_edit = document.createElement('input') // button
+    button_edit.type = 'button'
+    button_edit.value = 'edit'
+
+    button_edit.onclick = function () {  //onclick button "edit"
+      const new_age = document.createElement('input')  // create input
+      new_age.type = 'text'
+      const new_name = document.createElement('input')  // create input
+      new_name.type = 'text'
+      const new_sex = document.createElement('input')  // create input
+      new_sex.type = 'text'
+
+      cell_age.innerHTML = ''  // clear cell
+      cell_age.append(new_age)  // add input
+      cell_name.innerHTML = ''
+      cell_name.append(new_name)
+      cell_sex.innerHTML = ''
+      cell_sex.append(new_sex)
+
+      button_edit.value = 'save' // change name button "edit" on button "save"
+
+      button_edit.onclick = function () { // onclick button "save"      
+        const edit_age = new_age.value // variable edit_age = inpput value   
+        const edit_name = new_name.value
+        const edit_sex = new_sex.value
+        let user = {
+          name: edit_name,
+          sex: edit_sex,
+          age: edit_age
+        }
+        async function user_edit() {
+          await fetch('https://7r2d5vhfu8.execute-api.eu-west-2.amazonaws.com/dev/users/' + a, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+          })
+          button_edit.value = 'edit'
+          table_body.innerHTML = ""
+          get()
+        }
+        user_edit()
+      }
+    }
+    string.append(button_edit)
   }
 
   // ADD
@@ -54,7 +105,7 @@ async function get() {
     let user = {
       name: val_name,
       sex: val_sex,
-      age: Number.parseInt(val_age),
+      age: val_age
     }
 
     async function user_add() {
